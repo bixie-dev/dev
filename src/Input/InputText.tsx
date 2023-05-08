9adac4c46f4d04a36d2abb4064d2e8596f77bf94
RightShiner <brilight.shiner@gmail.com>
2023-05-08 18:59:46 +0200

    clear

--- src/Input/InputText.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/Login.module.css';
-
-const InputText = (props: { placeholder: string }) => (
-  <>
-    <input className="" placeholder={props.placeholder} type="text" />
-  </>
-);
-
-export { InputText };
